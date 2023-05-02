c0baa927aceb1f90fba0e6ea39996b3cda95811f
Kanagaraj Subramaniam <55802761+kanagaraj-subramaniam@users.noreply.github.com>
2023-05-02 13:39:09 +0530

    Minor changes

--- src/components/theaters/layoutsModal.js
+++ src/components/theaters/layoutsModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import Edit from "../../assets/images/editw.svg";
 import Close from "../../assets/images/closew.svg";
